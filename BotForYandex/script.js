481e4c0e1ff0b1fb9bc829bc776fa5217a35dbc4
Nargiza09 <82941458+Nargiza09@users.noreply.github.com>
2021-04-21 13:03:33 +0300

    Create script.js

---
+++ BotForYandex/script.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Bot for yandex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @icon         https://www.yandex.ru/s2/favicons?domain=yandex.ru
+// @grant        none
+// ==/UserScript==
+
+
+let keywords = ["Пион","Ландыш","Гортензия", "Роза"];
+let keyword = keywords[Math.floor(Math.random()*keywords.length)];
+let minisuggest__button = document.getElementsByClassName("mini-suggest__button")[0];
+if(minisuggest__button!=undefined){
+    document.getElementsByName("text")[0].value= keyword;
+    minisuggest__button.click();
+}else{
+    let links = document.links;
+    let nextPage = true;
+    let currentPage = document.getElementsByClassName("")[0].innerText;
+    for(let i=0; i<links.length; i++){
+        let link = links[i];
+        if(link.href.indexOf("divo-dacha.ru") != -1){
+            link.click();
+            nextPage = false;
+            break;
+        }
+    }
+    if(currentPage>10) location.href = "https://www.yandex.ru/";
+    else if(nextPage) pnnext.click();
+}
